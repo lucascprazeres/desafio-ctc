559e484c8dba3396b9a410cdf6262d4bcf5d7389
Lucas Prazeres <olucascostadp@gmail.com>
2022-06-12 10:45:51 -0300

    chore: setup database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  full_name = models.CharField(max_length=200)
+  mother_name = models.CharField(max_length=200)
+  cpf = models.CharField(max_length=11, blank=True, null=True, unique=True)
+  phone_number = models.CharField(max_length=20, blank=True, null=True)
+
+class Address(models.Model):
+  user_cpf = models.ForeignKey(User, on_delete=models.CASCADE)
+  city = models.CharField(max_length=50, blank=True, null=True)
+  neigborhood = models.CharField(max_length=50, blank=True, null=True)
+  street = models.CharField(max_length=50, blank=True, null=True)
+  house_number = models.CharField(max_length=5, blank=True, null=True)
