0fa134099b16da4621b53bd68440ee943179f80b
Lucas Prazeres <olucascostadp@gmail.com>
2022-06-13 09:21:17 -0300

    chore: setup postgresql

--- desafio_ctc/settings.py
+++ desafio_ctc/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'desafio_ctc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ctc',
+        'USER': 'postgres',
+        'PASSWORD': 'desafioctc',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
